f2c9f0f478f8320ba832e64397d883b2791d8347
Edd Remonts <eddremonts86@gmail.com>
2018-08-30 15:49:00 +0200

    change install script

--- LandoInstall/docker-lando.sh
+++ LandoInstall/docker-lando.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
-   
+
    sudo apt-get update
    sudo apt-get install -y git wget curl dpkg 
-   git clone https://github.com/docker/docker-install.git docker
-   cd docker
-   sudo chmod +x install.sh  
-   sudo apt-get install -f
-   ./install.sh 
-   #sudo systemctl status docker
+   sudo apt-get purge docker docker-engine docker.io lando
+
+   wget https://get.docker.com 
+   mv index.html dockerInstall.sh
+   sudo chmod +x dockerInstall.sh  
+   ./dockerInstall.sh 
+
    wget http://installer.kalabox.io/lando-latest-dev.deb
    sudo dpkg -i lando-latest-dev.deb
-   cd ..
-   sudo rm -r docker
-   lando  
-exit    
+
+   sudo usermod -aG docker $USER
+   rm lando-latest-dev.deb
+   rm dockerInstall.sh
+
+   lando init
+
+exit
+
